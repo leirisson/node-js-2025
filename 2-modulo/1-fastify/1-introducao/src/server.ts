4811f16251626e97ca29848068e3eda59f2867c5
leirisson <103783610+leirisson@users.noreply.github.com>
2025-11-17 13:48:58 -0400

    feat: initialize Fastify project with TypeScript support
    - Added package.json with Fastify and TypeScript dependencies
    - Created server.ts with a basic "Hello World" endpoint
    - Configured tsconfig.json for TypeScript compilation settings

---
+++ 2-modulo/1-fastify/1-introducao/src/server.ts
@@ -0,0 +1,17 @@
+import fastify from "fastify"
+
+const app = fastify()
+
+
+
+app.get('/hello', () => {
+    return "Hello Word"
+})
+
+
+app.listen({
+    port:3333
+})
+    .then(() => {
+        console.log("HTTP SERVER RUNING...💫")
+    })
